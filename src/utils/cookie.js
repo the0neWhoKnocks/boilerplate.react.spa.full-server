e310ced18eb6f187dac54c49bb43e0e72b1ea4d8
Trevor Lemon <duhlemman@gmail.com>
2018-08-25 13:37:18 -0700

    Logging cookie not being deleted
    Just had to use `expires` instead of `Max-Age`.

--- src/utils/cookie.js
+++ src/utils/cookie.js
@@ -24,5 +24,5 @@ export const getCookie = (name) => {
 };
 
 export const deleteCookie = (name) => {
-  document.cookie = `${ name }=; Max-Age=-99999999;`;
+  document.cookie = `${ name }=; expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
 };
